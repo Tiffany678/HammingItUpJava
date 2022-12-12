ccea19693a595fc2a296d7477018e39a72e51bad
Yingjie <52tiffany.yang@gmail.com>
2022-12-12 17:38:52 -0500

    completed

--- src/main/zipcode/Hamming.java
+++ src/main/zipcode/Hamming.java
@@ -1,10 +1,29 @@
 package zipcode;
 
+import org.junit.Assert;
+
 public class Hamming {
+    String s;
+    String s1;
+
     public Hamming(String s, String s1) {
+        this.s = s;
+        this.s1 = s1;
+        if (s.length() != s1.length()) {
+            throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
+        }
     }
 
+
     public int getHammingDistance() {
-        return -1;
+        String[] sArr = s.split("");
+        String[] s1Arr = s1.split("");
+
+        int counter = 0;
+        for (int i = 0; i < sArr.length; i++) {
+            if (!sArr[i].equals(s1Arr[i]))
+                counter++;
+        }
+        return counter;
     }
 }
